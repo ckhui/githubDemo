351ecf73899c75aa5edb702120a1b8d819971f64
ckhui <ckhanghui@gmail.com>
2017-08-11 17:06:38 +0800

    do nth

--- github/SecondViewController.swift
+++ github/SecondViewController.swift
@@ -10,26 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var lalal :Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addNewFunctionThatDoNth() {
+        //nth hahahahaa
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
